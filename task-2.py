a83beb45945e317f5089dd482dbee249b6021a66
Hemamalini <hemamalini04v@gmail.com>
2025-05-09 21:26:07 +0530

    Create task-2.py

---
+++ task-2.py
@@ -0,0 +1,10 @@
+import re
+
+def is_valid_email(email):
+    pattern = r'^\S+@\S+\.\S+$'
+    return re.match(pattern, email) is not None
+
+print(is_valid_email("user@example.com"))  
+print(is_valid_email("invalid@com"))       
+print(is_valid_email("bad email@.com"))    
+
